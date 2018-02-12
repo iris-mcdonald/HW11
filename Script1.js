d4eeee417daaffc26a8e5cd894dc3d1515372887
iris-mcdonald <32845453+iris-mcdonald@users.noreply.github.com>
2018-02-12 11:26:54 -0500

    Add files via upload

--- Script1.js
+++ Script1.js
@@ -26,40 +26,56 @@ $(document).ready(function () {
     }
 
     function clearSelection() {
-        console.log("clearSelection reached");
+        try {
+            console.log("clearSelection reached");
 
-        document.getElementById("celsius").checked = false;
-        document.getElementById("fahrenheit").checked = false;
-        document.getElementById("tempInput").value = "";
-        console.log (document.getElementById("celsius").checked);
-        console.log(document.getElementById("fahrenheit").checked);
-        document.getElementById("outputTemp").innerHTML = " ";
-        document.getElementById("errormsg").innerHTML = " ";
-        $("#convert").removeClass();
-        $("#convert").addClass("btn btn-lg");
+            document.getElementById("celsius").checked = false;
+            document.getElementById("fahrenheit").checked = false;
+            document.getElementById("tempInput").value = "";
+            console.log(document.getElementById("celsius").checked);
+            console.log(document.getElementById("fahrenheit").checked);
+            document.getElementById("outputTemp").innerHTML = " ";
+            document.getElementById("errormsg").innerHTML = " ";
+            $("#convert").removeClass();
+            $("#convert").addClass("btn btn-lg");
 
-        console.info("selection cleared");
-    }
+            console.info("selection cleared");
+        } catch (e) {
+            throw e;
+        };
+    }//end of clearSelection
 
     function convertTempFtoC(tempNumeric) {//convert Farhenheit to Celsius
-        var tempNumeric = (tempNumeric - 32) * (5 / 9);
-        if (isNaN(tempNumeric) == true) {
-            throw new UserException('InvalidDataOnFtoC');
-        } else {
-            return tempNumeric;
+        try {
+            var tempNumeric = (tempNumeric - 32) * (5 / 9);
+            if (isNaN(tempNumeric) == true) {
+                throw new UserException('InvalidDataOnFtoC');
+            } else {
+                return tempNumeric;
+            }
         }
-    }
+        catch(e) {
+            throw e;
+        };
+
+    }//end of convertTempFtoC(tempNumeric)
 
     function convertTempCtoF(tempNumeric) {//convert Celsius to Farhenheit
-        var tempNumeric = (tempNumeric * (9 / 5) + 32);
-        if (isNaN(tempNumeric) == true) {
-            throw new UserException('InvalidDataOnCtoF');
-        } else {
-            return tempNumeric;
-        }
-    }
+        try {
+            var tempNumeric = (tempNumeric * (9 / 5) + 32);
+            if (isNaN(tempNumeric) == true) {
+                throw new UserException('InvalidDataOnCtoF');
+            } else {
+                return tempNumeric;
+            }
+
+        } catch (e) {
+            throw e;
+        };
+    }//end of converTempCtoF
 
     function validateInput() {
+        try {
             var inputTemp = $('#tempInput').val();
             /*var temperature = document.getElementById("tempInput").value;*/
 
@@ -72,14 +88,22 @@ $(document).ready(function () {
                 return tempNumeric;
             }//end of if
 
+        } catch(e) {
+                throw e;
+            };
+
      }//end function validateInput
 
     function validateConversionType() {
-
-        if ((document.getElementById("fahrenheit").checked == false) &&
+        try {
+            if ((document.getElementById("fahrenheit").checked == false) &&
                 (document.getElementById("celsius").checked == false)) {
                 throw new UserException('NoTempTypeSelected');
             }
+        } catch (e) {
+            throw e    ;
+        }
+
     }//end function validateConversionType
 
     function startProcess() {
